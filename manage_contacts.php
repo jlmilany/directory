5e4d0046ef6629afb51d5f9bad11d13ebba1cad1
jlmilany <ejmilanay12@gmail.com>
2025-08-12 13:24:39 +0800

    updated manage_contacts

--- manage_contacts.php
+++ manage_contacts.php
@@ -3,6 +3,11 @@ require 'db.php';
 
 $message = "";
 
+// Start session if not already started
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
 // Check if user is logged in, if not redirect to login page
 if (!isset($_SESSION['user_id'])) {
     header("Location: login.php");
